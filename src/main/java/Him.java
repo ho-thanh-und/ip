fb304acc30ce0ac7d579938a72678bbca8e2558e
Chi Thanh <e1129981@u.nus.edu>
2025-01-24 03:48:10 +0800

    - Imported Scanner to retrieve input/output - Allows echoing: For whatever input the program receives, it will regurgitate it. If it's a "bye", the program will exit
    Mistakes occured:
    - The input variable of type String should be nested inside the while loop as to avoid an infinite loop
    - Similarly, it was necessary to add a while loop as to make the functionality of the if block work, or else it would also encounter an infinite loop

--- src/main/java/Him.java
+++ src/main/java/Him.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Him {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Him");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+          While loop is used for the echoing functionality:
+          - What the user inputs, the program outputs
+          - If it encounters a "bye", then the program will exit
+         */
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("See you later Aligator!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
