898304eea672acb81780e52df9b2d7fa8772604e
Chi Thanh <e1129981@u.nus.edu>
2025-01-24 01:13:54 +0800

    For Level-0: - Changed the Duke.java file to Him.java (will change itf) - Added a Ui class which consists of two methods - Used the Ui class in the Him.java file to greet and farewell users

---
+++ src/main/java/Him.java
@@ -0,0 +1,7 @@
+public class Him {
+    public static void main(String[] args) {
+        Ui ui = new Ui();
+        System.out.println(ui.getWelcome());
+        System.out.println(ui.getFarewell());
+    }
+}
