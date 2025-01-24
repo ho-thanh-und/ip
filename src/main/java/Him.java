fbd31059cae742ce54308fbe11c74705b9af320b
Chi Thanh <e1129981@u.nus.edu>
2025-01-24 12:30:00 +0800

    - Added Exceptions to catch errors for each specific function - A total of 4 errors were handled, including empty descriptions and invalid arguments - Added HimException.java to handle the errors
    Mistakes occurred:
    - Deciding the exceptions seemed to take up some time
    - The HimException.java class is never used. I think this is due to the fact that I did not separate the specific errors for it to handle

--- src/main/java/Him.java
+++ src/main/java/Him.java
@@ -16,57 +16,111 @@ public class Him {
          */
         while (true) {
             String input = scanner.nextLine();
-            String fstWord = input.split(" ")[0];
+            String[] words = input.split(" ");
+            String fstWord = words[0]; // Access the first word (safe since every input has at least one word)
+
             if (input.equals("thank you")) {
                 System.out.println("You're welcome, kind individual!");
                 break;
-                /*
-                - Used ArrayList for adjustable array size
-                 */
             } else if (input.equals("list")) {
-                int i = 1;
-                for (Task todo : todos) {
-                    System.out.printf("%d. %s%n", i++, todo);
+                if (todos.isEmpty()) {
+                    System.out.println("Your task list is empty.");
+                } else {
+                    int i = 1;
+                    for (Task todo : todos) {
+                        System.out.printf("%d. %s%n", i++, todo);
+                    }
                 }
             } else if (fstWord.equals("mark")) {
-                int num = Integer.parseInt(input.split(" ")[1]);
-                Task task = todos.get(num - 1);
-                task.markAsDone();
-                System.out.println("Congrats! Another task down: ");
-                System.out.println(todos.get(num - 1));
-            } else if (fstWord.equals("unmark")) {
-                int num = Integer.parseInt(input.split(" ")[1]);
-                Task task = todos.get(num - 1);
-                task.unmarkAsDone();
-                System.out.println("No worries! I'll unmark that for you: ");
-                System.out.println(todos.get(num - 1));
-            } else if (fstWord.equals("todo")) {
-                System.out.println("Gotcha bro! I've added this task:");
-                Task todo = new ToDo(input.substring(5));
-                System.out.println(todo);
-                todos.add(todo);
-                System.out.printf("Now you have %d tasks in your list%n", todos.size());
-            } else {
-                String substring = input.substring(input.indexOf('/') + 4);
-                if (fstWord.equals("deadline")) {
-                    System.out.println("Gotcha bro! I've added this task:");
-                    Task deadline = new Deadline(input.substring(9, input.indexOf('/')),
-                            substring);
-                    System.out.println(deadline);
-                    todos.add(deadline);
-                    System.out.printf("Now you have %d tasks in your list%n", todos.size());
-                } else if (fstWord.equals("event")) {
-                    System.out.println("Gotcha bro! I've added this task:");
-                    Task event = new Event(input.substring(6, input.indexOf('/')),
-                            substring);
-                    System.out.println(event);
-                    todos.add(event);
-                    System.out.printf("Now you have %d tasks in your list%n", todos.size());
-                } else {
-                    todos.add(new Task(input));
-                    System.out.println("added: " + input);
+                try {
+                    if (words.length < 2) {
+                        throw new IllegalArgumentException("What task number would you like me to mark?");
+                    }
+                    int num = Integer.parseInt(words[1]);
+                    if (num < 1 || num > todos.size()) {
+                        throw new IllegalArgumentException("Oops! I don't see that task");
+                    }
+                    Task task = todos.get(num - 1);
+                    task.markAsDone();
+                    System.out.println("Congrats! Another task down: ");
+                    System.out.println(task);
+                } catch (NumberFormatException e) {
+                    System.out.println("Oops! It should be an integer!");
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
+            } else if (fstWord.equals("unmark")) {
+                try {
+                    if (words.length < 2) {
+                        throw new IllegalArgumentException("What task number would you like me to unmark?");
+                    }
+                    int num = Integer.parseInt(words[1]);
+                    if (num < 1 || num > todos.size()) {
+                        throw new IllegalArgumentException("Oops! It should be an integer!");
+                    }
+                    Task task = todos.get(num - 1);
+                    task.unmarkAsDone();
+                    System.out.println("No worries! I'll unmark that for you: ");
+                    System.out.println(task);
+                } catch (NumberFormatException e) {
+                    System.out.println("Oops! It should be an integer!");
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else if (fstWord.equals("todo")) {
+                try {
+                    if (input.trim().length() <= 5) {
+                        throw new IllegalArgumentException("Oh dear! The todo seems to be empty");
+                    }
+                    Task todo = new ToDo(input.substring(5).trim());
+                    todos.add(todo);
+                    System.out.println("Gotcha bro! I've added this task:");
+                    System.out.println(todo);
+                    System.out.printf("Now you have %d tasks in your list%n", todos.size());
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else if (fstWord.equals("deadline")) {
+                try {
+                    if (!input.contains("/by")) {
+                        throw new IllegalArgumentException("A deadline must include '/by' followed by the due date");
+                    }
+                    String description = input.substring(9, input.indexOf('/')).trim();
+                    String by = input.substring(input.indexOf('/') + 4).trim();
+                    if (description.isEmpty() || by.isEmpty()) {
+                        throw new IllegalArgumentException("The deadline description or due date can't be empty bro");
+                    }
+                    Task deadline = new Deadline(description, by);
+                    todos.add(deadline);
+                    System.out.println("Gotcha bro! I've added this task:");
+                    System.out.println(deadline);
+                    System.out.printf("Now you have %d tasks in your list%n", todos.size());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Uh Oh! " + e.getMessage());
+                }
+            } else if (fstWord.equals("event")) {
+                try {
+                    if (!input.contains("/at")) {
+                        throw new IllegalArgumentException("An event must include '/at' followed by the event time");
+                    }
+                    String description = input.substring(6, input.indexOf('/')).trim();
+                    String at = input.substring(input.indexOf('/') + 4).trim();
+                    if (description.isEmpty() || at.isEmpty()) {
+                        throw new IllegalArgumentException("The event description or time can't be empty bro");
+                    }
+                    Task event = new Event(description, at);
+                    todos.add(event);
+                    System.out.println("Gotcha bro! I've added this task:");
+                    System.out.println(event);
+                    System.out.printf("Now you have %d tasks in your list%n", todos.size());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Uh Oh! " + e.getMessage());
+                }
+            } else {
+                todos.add(new Task(input));
+                System.out.println("added: " + input);
             }
         }
     }
 }
+
---
+++ src/main/java/HimException.java
@@ -0,0 +1,5 @@
+public class HimException extends Exception {
+    public HimException(String msg) {
+        super(msg);
+    }
+}
