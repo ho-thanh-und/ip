4b96a1dab6707312e3df14ec7aec5d59792d167e
Chi Thanh <e1129981@u.nus.edu>
2025-01-30 03:56:19 +0700

    Update the Tasks subclasses

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -3,8 +3,9 @@ public class ToDo extends Task {
      * Creates a To-Do object with its description
      * @param description description
      */
-    public ToDo(String description) {
-        super(description);
+    public ToDo(String description, boolean isDone) {
+
+        super(description, isDone);
     }
 
     /**
@@ -14,5 +15,9 @@ public class ToDo extends Task {
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    public String toFile() {
+        return "T | " + super.toFile();
     }
 }
