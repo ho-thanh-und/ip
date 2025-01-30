d470447d5b9ddcc52a76aa2822e49dcacc91a7be
Chi Thanh <e1129981@u.nus.edu>
2025-01-31 03:07:17 +0700

    Divide files by packages

---
+++ src/main/java/him/task/ToDo.java
@@ -0,0 +1,30 @@
+package him.task;
+
+
+public class ToDo extends Task {
+
+    public ToDo(String description) {
+        super(description);
+    }
+
+    /**
+     * Creates a To-Do object with its description
+     * @param description description
+     */
+    public ToDo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    /**
+     * Gives a string representation of the To-do object
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
+    public String toFile() {
+        return "T | " + super.toFile();
+    }
+}
